9f78856c2327369ed80fd3ee1dbe9fba81ede256
Guilherme Raimundo <130239914+guihlerwe@users.noreply.github.com>
2025-06-26 14:22:41 -0300

    ! aula

---
+++ SpringbootAula/springtest/src/main/java/com/springtest/springtest/Cliente.java
@@ -0,0 +1,14 @@
+package com.springtest.springtest;
+
+public class Cliente{
+    public static long id;
+    public String  nome;
+    public String cpf;
+    public String telefone;
+    public String enedereco;
+    public int idade;
+
+    public Cliente(){
+        id+=1;
+    }
+}
