3c95885a0216e279e4f5f49d7c0567b34d2a8c2d
guilherme <guiihlerwe@icloud.com>
2025-07-03 11:14:49 -0300

    Aula de PHP 03/08 1-2

---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="processar.php" method="GET>
+        <label for= "nome">insira seu nome</label> 
+        <input type= "text" name = "nome" id = "nome">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
